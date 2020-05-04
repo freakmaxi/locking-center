a0f4b4bae69e45dbad12ca3355fcf6d3095856a1
Tuna Celik <tuna@jakpark.com>
2020-05-04 02:16:58 +0200

    Updated create_release script

--- -build-/executable/create_release.sh
+++ -build-/executable/create_release.sh
@@ -9,13 +9,12 @@ mkdir releases/linux
 mkdir releases/macosx
 mkdir releases/windows
 
-major=$(date +%y)
 buildNo=$(($(date +%s)/345600))
 
-export RELEASE_VERSION="$major.1.$buildNo"
+export RELEASE_VERSION="0.1.$buildNo"
 
 echo ""
-echo "Building Locking Center (v$RELEASE_VERSION)"
+echo "Building Locking Center Server (v$RELEASE_VERSION)"
 cd ../../mutex
 echo "  > compiling linux x64 release"
 GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/linux/locking-center
@@ -23,3 +22,13 @@ echo "  > compiling macosx x64 release"
 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/macosx/locking-center
 echo "  > compiling windows x64 release"
 GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/windows/locking-center.exe
+
+echo ""
+echo "Building Locking Center CLI (v$RELEASE_VERSION)"
+cd ../cli
+echo "  > compiling linux x64 release"
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/linux/lc-cli
+echo "  > compiling macosx x64 release"
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/macosx/lc-cli
+echo "  > compiling windows x64 release"
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$RELEASE_VERSION" -o ../-build-/executable/releases/windows/lc-cli.exe
